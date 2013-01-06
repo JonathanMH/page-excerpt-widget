d085a77d42426bee71de3848b212eec0fe0b4f0a
Jonathan M. Hethey <public@jonathanmh.com>
2013-01-06 03:38:08 +0100

    cleanup, changelog

--- jmh_pew.php
+++ jmh_pew.php
@@ -1,7 +1,7 @@
 <?php
 /*
 	Plugin Name: Page Excerpt Widget
-	Plugin URI: http://jonathanmh.com/blog/117-wordpress-page-excerpt-widget
+	Plugin URI: http://jonathanmh.com/wordpress-page-excerpt-widget/
 	Description: Plugin for displaying a page excerpt in a widget area
 	Author: Jonathan M. Hethey
 	Version: 0.2
@@ -36,7 +36,6 @@ class PageExcerptWidget extends WP_Widget {
 			// trim to last whitespace in string
 			$text = substr($text, 0, $last_whitespace);
 			// append dots
-			$text .=' [...]';
 			return $text;
 		}
 		// if the text is shorter than the trim limit, pass it on
@@ -50,17 +49,28 @@ class PageExcerptWidget extends WP_Widget {
 		echo $before_widget;
 		$page_data = get_page($instance['page_id']);
 		$title = $page_data->post_title;
+		$permalink = get_permalink($instance['page_id']);
 		if (!empty($title) && $instance['display_title'] == 'on') {
 			echo $before_title;
 			if ($instance['link_title']){
-				echo '<a href="'. get_permalink($instance['page_id']) .'">'. $title . '</a>';
+				echo '<a href="'. $permalink .'">'. $title . '</a>';
 			}
 			else {
 				echo $title;
 			}
 			echo $after_title;
 		};
+			
+		echo '<p>';
 		echo $this->pew_trim($page_data->post_content, $instance['excerpt_length']);
+		
+		if ($instance['dot_excerpt'] == 'on'){
+			echo ' [...]';
+		}
+		echo '</p>';
+		if ($instance['display_read_more'] == 'on'){
+			echo ' <a href="'. $permalink .'">'. $instance['read_more_label'] .'</a>';
+		}
 		
 		/* debugging
 			echo '<pre>';
@@ -75,13 +85,23 @@ class PageExcerptWidget extends WP_Widget {
 		$instance = $old_instance;
 		$instance['page_id'] = strip_tags($new_instance['page_id']);
 		$instance['excerpt_length'] = strip_tags($new_instance['excerpt_length']);
+		$instance['dot_excerpt'] = strip_tags($new_instance['dot_excerpt']);
 		$instance['display_title'] = strip_tags($new_instance['display_title']);
 		$instance['link_title'] = strip_tags($new_instance['link_title']);
+		$instance['display_read_more'] = strip_tags($new_instance['display_read_more']);
+		$instance['read_more_label'] = strip_tags($new_instance['read_more_label']);
 		return $instance;
 	}
 
 	function form($instance) {
-		$default = 	array( 'title' => __('Page Excerpt Widget'), 'excerpt_length' => __(500), 'display_title' => __('on') );
+		$default = 	array(
+			  'title'				=> 'Page Excerpt Widget'
+			, 'excerpt_length'		=> 500
+			, 'dot_excerpt'			=> 'on'
+			, 'display_title'		=> 'on'
+			, 'display_read_more'	=> 'on'
+			, 'read_more_label'		=> 'read full page'
+						 );
 		$instance = wp_parse_args( (array) $instance, $default );
 		$page_id = $this->get_field_name('page_id');
 		_e("Page to display: " );
@@ -105,6 +125,7 @@ class PageExcerptWidget extends WP_Widget {
 				?>
 			</select>
 		<?php
+
 		$field_excerpt_length_id = $this->get_field_id('excerpt_length');
 		$field_excerpt_length = $this->get_field_name('excerpt_length');
 		echo "\r\n"
@@ -119,6 +140,30 @@ class PageExcerptWidget extends WP_Widget {
 			.'" value="'
 			.esc_attr( $instance['excerpt_length'] )
 			.'" /></p>';
+		
+		$field_dot_excerpt_id = $this->get_field_id('dot_excerpt');
+		$field_dot_excerpt = $this->get_field_name('dot_excerpt');
+		
+		if ($instance['dot_excerpt'] == 'on'){
+			$checked = 'checked="checked"';
+		}
+		else {
+			$checked = '';
+		}
+		
+		echo "\r\n"
+			.'<p><input type="checkbox" id="'
+			.$field_dot_excerpt_id
+			.'" name="'
+			.$field_dot_excerpt
+			.'" value="on"'
+			.$checked
+			.'/> <label for="'
+			.$field_dot_excerpt_id
+			.'">'
+			.__('Show dots after excerpt `[...]`')
+			.' </label></p>';
+		
 		$field_display_title_id = $this->get_field_id('display_title');
 		$field_display_title = $this->get_field_name('display_title');
 		
@@ -141,11 +186,10 @@ class PageExcerptWidget extends WP_Widget {
 			.$field_display_title_id
 			.'">'
 			.__('Display Page Title')
-			.': </label></p>';
+			.' </label></p>';
 		
 		$field_link_title_id = $this->get_field_id('link_title');
 		$field_link_title = $this->get_field_name('link_title');
-		
 		
 		if ($instance['link_title'] == 'on'){
 			$checked = 'checked="checked"';
@@ -165,7 +209,48 @@ class PageExcerptWidget extends WP_Widget {
 			.$field_link_title_id
 			.'">'
 			.__('Link Page Title')
-			.': </label></p>';
+			.' </label></p>';
+		
+		$field_display_read_more_id = $this->get_field_id('display_read_more');
+		$field_display_read_more = $this->get_field_name('display_read_more');
+		
+		if ($instance['display_read_more'] == 'on'){
+			$checked = 'checked="checked"';
+		}
+		else {
+			$checked = '';
+		}
+		
+		echo "\r\n"
+			.'<p><input type="checkbox" id="'
+			.$field_display_read_more_id
+			.'" name="'
+			.$field_display_read_more
+			.'" value="on"'
+			.$checked
+			.'/> <label for="'
+			.$field_display_read_more_id
+			.'">'
+			.__('Display Read More Link')
+			.' </label></p>';
+		
+		$field_read_more_label_id = $this->get_field_id('read_more_label');
+		$field_read_more_label = $this->get_field_name('read_more_label');
+		echo "\r\n"
+			.'<p><label for="'
+			.$field_read_more_label_id
+			.'">'
+			.__('Read More Label')
+			.': </label><input type="text" id="'
+			.$field_read_more_label_id
+			.'" name="'
+			.$field_read_more_label
+			.'" value="'
+			.esc_attr( $instance['read_more_label'] ).'"'
+			.'placeholder="read full page"'
+			.'/></p>';
+		
+		
 	}
 	
 /* class end */
--- readme.txt
+++ readme.txt
@@ -2,9 +2,9 @@
 Contributors: JonathanMH
 Donate link: http://example.com/
 Tags: page, page excerpt, widget, read more link
-Requires at least: unkown
-Tested up to: 3.4
-Stable tag: 0.1
+Requires at least: 2.8
+Tested up to: 3.5
+Stable tag: 0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -16,18 +16,21 @@ I have created a little WordPress plugin in the shape of a widget, which allows 
 I wrote this to replace a little theme hack I did for a client, where the front page was supposed to have an excerpt of a page on the front page, of course linking to the full page. Instead of keeping it that way and hard coding the page-id, I wanted something where the end user could change which page was supposed to be linked. I hope it will be of use to someone.
 
 Right now you can:
+
 * Define the amount of characters to use as an excerpt
 * Select the page from all existing pages
 * Link the title of the page, to the page
 * Append a link to the page
 * Decide a custom label for the read more link
+
 I plan to increase the functionality with:
+
 * the possibility for multiple instances of the widget
 * some internationalisation
 
 == Installation ==
 
-1. Upload the `jmh_page_excerpt_widget` directory to `/wp-content/plugins/` on your WordPress installation.
+1. Upload the `page_excerpt_widget` directory to `/wp-content/plugins/` on your WordPress installation.
 2. Activate the plugin through the 'Plugins' menu in WordPress.
 3. Go to 'Appearance > Widgets' to place the Page Excerpt Widget in one of your sidebar areas.
 
@@ -35,7 +38,7 @@ I plan to increase the functionality with:
 
 = Can I have multiple page excerpt widgets active at the same time? =
 
-Not yet.
+Yes.
 
 == Screenshots ==
 
@@ -44,8 +47,23 @@ Not yet.
 
 == Changelog ==
 
+= 0.2 =
+2013-1-6
+
+Rewrite for multiple instances
+
+* WP 2.8 is a requirement now
+
 = 0.1 =
-First submitted version.
+2012-7-11
+
+Initial release
+
+* Define the amount of characters to use as an excerpt
+* Select the page from all existing pages
+* Link the title of the page, to the page
+* Append a link to the page
+* Decide a custom label for the read more link
 
 == Suggestions welcome ==
 Since this is my first plugin and widget, suggestions, improvements and more are very welcome. I've also published the code on github, for easier discussion and improvement. https://github.com/JonathanMH/page-excerpt-widget
